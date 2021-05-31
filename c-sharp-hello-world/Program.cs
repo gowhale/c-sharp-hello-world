a96f102314a52bf152be74c6753d4a601c5c7180
Gabriel Whale <whalegh@cardiff.ac.uk>
2021-05-31 14:54:01 +0100

    First minor change to intial program.

--- c-sharp-hello-world/Program.cs
+++ c-sharp-hello-world/Program.cs
@@ -6,7 +6,7 @@ namespace c_sharp_hello_world
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! This is my first repo using C#!");
         }
     }
 }
